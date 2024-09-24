21e7de65f17d992c7875634a3518dd0ad5ed3520
suranga-jayalath <suranga@shoutoutlabs.io>
2024-09-24 14:39:46 +0530

    updated

--- src/slack.ts
+++ src/slack.ts
@@ -460,7 +460,7 @@ async function buildMessageContent(input: TInput): Promise<{
               },
               {
                 type: 'mrkdwn',
-                text: `<${messageURL}|${messageURL}>`,
+                text: `<${messageURL}>`,
               },
             ],
           },
